a9e4163e1a956a87626bbc06dd8720baa28436a0
Derrick1451 <haylederrick14@gmail.com>
2024-04-15 17:37:35 +0300

    Update user and rsvp controllers

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,17 @@ class Api::v1::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token},status: :created
+        else
+            render json: {errors: 'invalid user name or password'}, status: :not_acceptable
+        end
+    end
+        private
+        def user_params
+            params.require(:user).permit(:name, :email, :password)
+        end
 end
